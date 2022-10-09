7fac279861b5bf8213a29573f0ac779cadf437c3
Dragoș Efrem <101568589+dragosefrem@users.noreply.github.com>
2022-10-09 21:06:24 +0300

    Backend project structure initialisation

---
+++ backend/src/main/java/efrem/datamanager/DatamanagerApplication.java
@@ -0,0 +1,16 @@
+package efrem.datamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DatamanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamanagerApplication.class, args);
+	}
+
+}
